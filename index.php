38943bcf0214a55e93784d8af74bed77695b1a3e
Yak036 <ramcesvedes@gmail.com>
2023-11-17 00:08:13 -0400

    Se agregaron las barras de navegacion junto htacces(rutas amigables) la primera interfaz de registroviviendas tiene parte de la funcionalidad backend

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
-echo "Waos";
+
+
+$url = isset($_GET['url']) ? $_GET['url'] : '';
+//path = Ruta raiz de la pagina (__DIR__ la da)
+$path = __DIR__ . "/app/views/";
+// file: archivo al cual se dirige el usuario;
+$file = $path . $url . ".php";
+
+if (!empty($url)) {
+  if (is_file($file)) {
+    require $file;
+  } else {
+    echo "No se encontro la pagina";
+  }
+} else {
+  header("location: inicio");
+}
---
+++ public/js/alert.js
@@ -0,0 +1,26 @@
+
+
+if (document.querySelector(".exitoAggVivienda")) {
+  let timerInterval
+Swal.fire({
+  icon: 'success',
+  title: 'Se subieron los datos con exito',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})  
+}
